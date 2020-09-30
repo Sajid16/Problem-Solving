7d07190cc0500cb2a81ef1b2a261e4c6dc167b33
Sajid16 <sajid.mahboob16@gmail.com>
2020-10-01 03:17:01 +0600

    practice code for enosis is in test.py file

---
+++ test.py
@@ -0,0 +1,47 @@
+# reverse of a sentence
+
+# string = input()
+#
+# list1 = string.split()
+# for i in range(len(list1)-1, -1, -1):
+#     print(list1[i], end=' ')
+
+####################################################################
+
+
+# reverse of the words of a sentence
+
+# string = input()
+#
+# list1 = string.split()
+# list2, result = [], []
+# for i in range(0, len(list1)):
+#     list2 = list(list1[i])
+#     for j in range(len(list2) - 1, -1, -1):
+#         result.append(list2[j])
+#     result.append(' ')
+#     # print(list2, end=' ')
+#
+# for letters in result:
+#     print(letters, end='')
+
+#################################################
+
+# for i in range(200, 0, -1):
+#     if i % 3 == 0:
+#         print(i, end=' ')
+
+
+##############################################
+
+# numbers = list(map(int, input().split(',')))
+# result = []
+# for i in range(0, len(numbers)):
+#     for j in range(i+1, len(numbers)):
+#         if numbers[j] == numbers[i] and numbers[j] not in result:
+#             result.append(numbers[i])
+#     # print(i)
+# print(result)
+
+#############################################
+
